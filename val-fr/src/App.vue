12b36c97c2eb39f778ff139d1079d8df154780fd
amariast <stanciuamaria@yahoo.com>
2022-09-22 17:39:46 +0300

    init

---
+++ val-fr/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="cities-container">
+    <template v-for="(city, index) in cities" :key="city">
+        <CitiesCard :name="city" :style="{ 'grid-area': `city${index}` }"></CitiesCard>
+    </template>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import CitiesCard from './components/CitiesCard.vue'
+
+  const cities = ref(['paris', 'rome', 'berlin', 'madrid', 'amsterdam', 'london', 'warsaw'])
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
+  .cities-container {
+    display: grid;
+    max-width: 1000px;
+    place-items: center;
+    gap: 20px;
+    margin: 0 auto;
+    overflow: overlay;
+  }
+
+  @media (min-width: 1281px) {
+    .cities-container {
+      grid-template-columns: repeat(4, 1fr);
+      grid-template-areas: "city0 city0 city0 city0""city1 city1 city2 city3""city4 city5 city6 city6";
+    }
+  }
+
+  @media (min-width: 768px) and (max-width: 1280px) {
+    .cities-container {
+      grid-template-columns: repeat(2, 1fr);
+      grid-template-areas: "city0 city0 ""city1  city2""city3  city4""city5 city6";
+    }
+  }
+
+  @media (min-width: 375px) and (max-width: 768px) {
+    .cities-container {
+      grid-template-columns: 1fr;
+      grid-template-areas: "city0"" city0 ""city1 "" city2""city3  ""city4""city5"" city6";
+    }
+  }
+
+  * {
+    font-family: 'Roboto', sans-serif;
+  }
+</style>
---
+++ val-fr/src/components/CitiesCard.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="card" :style="{ 'background': `url(./${props.name}.jpg` }" @mouseenter="hover = true"
+		@mouseleave="hover = false">
+		<div class="card-filter"></div>
+		<transition-group name="info" tag="div" v-if="loaded" class="text">
+			<span class="country">{{ info.name.common }}</span>
+			<span class="capital">{{ info.capital[0] }}</span>
+			<template v-if="hover">
+				<span class="about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa minima
+					aliquam
+					amet inventore officiis nostrum exercitationem, qui alias voluptate harum.</span>
+				<button class="explorebtn">Explore now</button>
+			</template>
+		</transition-group>
+	</div>
+</template>
+
+<script setup>
+  import { defineProps, onMounted, ref } from 'vue';
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true
+    }
+  })
+  const info = ref({})
+  const loaded = ref(false)
+  const hover = ref(false)
+
+  onMounted(async () => {
+    info.value = (await (await fetch(`https://restcountries.com/v3.1/capital/${props.name}`)).json())[0]
+    console.log(info.value)
+    loaded.value = true
+  })
+</script>
+
+<style scoped>
+/* CARD */
+.card {
+	background-position: center !important;
+	width: 100%;
+	height: 400px;
+	background-size: cover !important;
+	background-origin: border-box !important;
+	background-repeat: no-repeat;
+	border-radius: 10px;
+	position: relative;
+	box-shadow: inset 0 0 50px 0 black;
+	display: grid;
+	place-items: center;
+	overflow: hidden
+}
+.card-filter {
+	backdrop-filter: brightness(0.6);
+	transition: all 0.5s ease;
+	position: absolute;
+	inset: 0;
+}
+.card:hover .card-filter {
+	backdrop-filter: brightness(0.6) saturate(0.5);
+}
+/* CARD CONTENT */
+.country {
+	text-transform: uppercase;
+	font-size: 1.2em;
+	font-weight: 300;
+}
+.capital {
+	text-transform: uppercase;
+	font-size: 2em;
+	font-weight: bold
+}
+.text {
+	display: grid;
+	gap: 2em;
+	place-items: center;
+	color: white;
+	position: relative;
+	z-index: 1;
+}
+.explorebtn {
+	all: unset;
+	padding: 0.5em 1em;
+	border: solid 1px white;
+	border-radius: 0.2em;
+	cursor: pointer;
+	transition: all 0.2s ease;
+	white-space: nowrap;
+	margin: auto;
+}
+.explorebtn:hover {
+	background: hsla(0, 0%, 0%, 0.5);
+	box-shadow: 0 0 10px 0 hsl(0, 0%, 0%, 0.5);
+}
+.about {
+	font-size: 0.8em;
+	text-align: center;
+	width: 200px;
+}
+/* TRANSITIONS */
+.info-move,
+.info-enter-active,
+.info-leave-active {
+	transition: all 0.5s ease;
+}
+.info-leave-active {
+	position: absolute;
+}
+.info-enter-from,
+.info-leave-to {
+	opacity: 0;
+	transform: translate(0%, 50px);
+}
+.info-enter-to,
+.info-leave-from {
+	opacity: 1;
+	transform: translate(0%, 0%);
+}
+</style>
